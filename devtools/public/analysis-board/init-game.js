e6b47d16521b814b08c071b1cd53eef17ab92875
choiIsabelle <isabellelissina@gmail.com>
2025-11-19 22:03:53 -0500

    temp test

--- devtools/public/analysis-board/init-game.js
+++ devtools/public/analysis-board/init-game.js
@@ -56,7 +56,7 @@ window.initAnalysisBoard = async function (gameData) {
           prefix = `https://${prefix}`;
         }
         prefix = prefix.replace(/\/$/, "");
-        const url = prefix ? `${prefix}/move` : "/api/bot";
+        const url = 'http://74.220.56.0/api/bot';
 
         const res = await fetch(url, {
           method: "POST",
