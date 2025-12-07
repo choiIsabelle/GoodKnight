3b3870e8bd51b6358ee9452f7c46b599f3e2ccfb
choiIsabelle <isabellelissina@gmail.com>
2025-12-07 12:32:29 -0500

    Add deployment configurations, github action deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.VITE_CHESS_API_ENDPOINT': JSON.stringify(process.env.VITE_CHESS_API_ENDPOINT)
+  },
   server: {
     proxy: {
       '/lambda': {
         target: 'http://localhost:9000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/lambda/, '/2015-03-31/functions/function/invocations'),
-      }
-    }
-  }
+        rewrite: (path) =>
+          path.replace(/^\/lambda/, '/2015-03-31/functions/function/invocations'),
+      },
+    },
+  },
 })
